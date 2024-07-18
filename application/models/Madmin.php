ddb9030844955d840c0190b2ae825f6d61d8e9d9
Anastasya <work.anastasyaa@gmail.com>
2024-07-18 20:19:27 +0800

    p

--- application/models/Madmin.php
+++ application/models/Madmin.php
@@ -24,12 +24,13 @@ class Madmin extends CI_Model {
         $this->db->from('tb_pengguna');
         $this->db->join('tb_pengelola', 'tb_pengguna.id_user = tb_pengelola.id_user');
         $this->db->join('tb_tempatwisata', 'tb_pengelola.id_wisata = tb_tempatwisata.id_wisata');
-        $this->db->where('tb_pengguna.level', 'user');
+        $this->db->where('tb_pengguna.level', 'pengelola');
         $this->db->where('tb_tempatwisata.diterima', 'ya');
         $query = $this->db->get();
         return $query->result();
     }
 
+    // belom
     public function data_pengajuan_sponsor() {
         $this->db->select('tb_pengguna.*, tb_tempatwisata.*');
         $this->db->from('tb_pengguna');
