51fe9579ebef364eea09036c26e38fe5c195d79e
aryanathaVJ <138412842+aryanathaVJ@users.noreply.github.com>
2024-07-30 14:31:12 +0800

    c

--- application/views/pengguna/cetak_pdf.php
+++ application/views/pengguna/cetak_pdf.php
@@ -3,7 +3,7 @@
 <head>
     <title>Cetak Tiket</title>
     <style>
-        <style>
+       
         body {
             font-family: Arial, sans-serif;
             display: flex;
@@ -21,7 +21,12 @@
             padding: 20px;
             position: relative;
             overflow: hidden;
+<<<<<<< Updated upstream
 
+=======
+            background-clip: padding-box; 
+            box-sizing: border-box;
+>>>>>>> Stashed changes
         }
         .ticket-container::before, .ticket-container::after {
             content: '';
@@ -32,6 +37,10 @@
             left: -50%;
             background: radial-gradient(circle, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
             transform: rotate(30deg);
+<<<<<<< Updated upstream
+=======
+            pointer-events: none;
+>>>>>>> Stashed changes
         }
         .ticket-header {
             text-align: center;
@@ -39,7 +48,11 @@
             background: linear-gradient(90deg, #4facfe 0%, #00f2fe 100%);
             padding: 10px;
             border-radius: 10px 10px 0 0;
+<<<<<<< Updated upstream
             color: #0099ff;
+=======
+            color: #3399ff;
+>>>>>>> Stashed changes
         }
         .ticket-details {
             margin-top: 20px;
@@ -69,4 +82,34 @@
         }
     </style>
 </head>
+<<<<<<< Updated upstream
+=======
+<body>
+    <div class="ticket-container">
+        <div class="ticket-header">
+            <h1><?php echo isset($nama_wisata) ? $nama_wisata : 'Data tidak tersedia'; ?></h1>
+            <p>Tiket Masuk</p>
+        </div>
+        <div class="ticket-details">
+            <table>
+                <tr>
+                    <th>ID Pesanan</th>
+                    <td><?= isset($id_pesanan) ? $id_pesanan : 'Data tidak tersedia'; ?></td>
+                </tr>
+                <tr>
+                    <th>Harga Tiket</th>
+                    <td>Rp. <?= isset($total_harga) ? number_format($total_harga, 0, ',', '.') : 'Data tidak tersedia'; ?></td>
+                </tr>
+                <tr>
+                    <th>Tanggal Kunjungan</th>
+                    <td><?= isset($tgl_kunjungan) ? $tgl_kunjungan : 'Data tidak tersedia'; ?></td>
+                </tr>
+            </table>
+        </div>
+        <div class="ticket-footer">
+            <p>Terima kasih atas kunjungan Anda!</p>
+        </div>
+    </div>
+</body>
+>>>>>>> Stashed changes
 </html>
